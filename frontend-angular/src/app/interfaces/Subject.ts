9922332447896cbf7d7be7d8adda4b24f47e59ba
juliatribo <julstribo@gmail.com>
2021-11-16 16:14:15 +0100

    frontend-delete

--- frontend-angular/src/app/interfaces/Subject.ts
+++
@@ -1,5 +0,0 @@
-export interface Subject{
-    _id?: string;
-    name: string;
-    credits: number
-}
--- frontend-angular/src/app/interfaces/Teacher.ts
+++
@@ -1,7 +0,0 @@
-import { Subject } from "./Subject";
-export interface Teacher{
-    _id?: string;
-    name: string;
-    subjects: Subject['_id'];
-    age: number
-}
