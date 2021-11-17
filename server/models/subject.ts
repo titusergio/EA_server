339679eaf98652f11bebdbfe77fe8d9d5b17d238
juliatribo <julstribo@gmail.com>
2021-11-17 11:04:51 +0100

    models changed

--- server/models/subject.ts
+++ server/models/subject.ts
@@ -5,16 +5,14 @@ import { StudentI } from "./student";
 // 1. Create an interface representing a document in MongoDB.
  export interface SubjectI extends Document {                                                                     
     name: string,
-    numberStudents: number,                                                                                                  
-    students: Array<ObjectId>      
+    numberStudents: number     
     };
   
     
   // 2. Create a Schema corresponding to the document interface.
 const SubjectSchema = new Schema<SubjectI>({
     name: { type: String, required: true },
-    numberStudents: { type: Number, required: true },
-    students: [{type: Schema.Types.ObjectId, ref:'StudentI',required:false}]
+    numberStudents: { type: Number, required: true }
     
   });
 
