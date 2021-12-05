d5f7ef8936749d30f52232720635e18f3ca5cfa9
decaptus <72303216+decaptus@users.noreply.github.com>
2021-12-05 14:05:52 +0100

    mensajes añadidos a joi

--- server/middleware/validatiom.ts
+++ server/middleware/validatiom.ts
@@ -1,12 +1,26 @@
-import joi from "joi";
+import Joi from "Joi";
 
-const validationSchema = joi.object({
-    name: joi.string(),
-    lastName: joi.string(),
-    email: joi.string().email().lowercase().required(),
-    password: joi.string().min(4).required(),
-    age: joi.number(),
-    subjects: joi.array()
+const validationSchema = Joi.object({
+    name: Joi.string(),
+    lastName: Joi.string(),
+    email: Joi.string()
+        .email()
+        .lowercase()
+        .required()
+        .messages({
+            'any.required': `"a" is a required field`
+          }),
+    password: Joi.string()
+        .min(4)
+        .required()
+        .messages({
+            'string.empty': `"a" cannot be an empty field`,
+            'string.min': `"a" should have a minimum length of {#limit}`,
+            'any.required': `"a" is a required field`
+          }),
+    confirmPassword: Joi.string(),
+    age: Joi.number(),
+    subjects: Joi.array()
 })
 
 export default validationSchema;
