39449ff5c7107ea33905f9a7dd8a478dcdc99fc6
tatianatkachuk <tatiana.tkachuk@estudiantat.upc.edu>
2021-12-14 21:43:59 +0100

    Algunos errores arreglados

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,7 @@ import studentsRoutes from './routes/students';
 import subjectRoutes from './routes/subject';
 import teacherRoutes from './routes/teacher';
 import flatsRoutes from './routes/flats';
+import markersRoutes from './routes/markers';
 
 dotenv.config();
 
@@ -25,12 +26,10 @@ app.use('/students', studentsRoutes);
 app.use('/subjects',subjectRoutes);
 app.use('/teachers',teacherRoutes);
 app.use('/flats',flatsRoutes);
-
+app.use('/markers',markersRoutes);
 
 mongoose.connect(`${process.env.CONNECTION_URL}`)                         // have to use a template string and interpolate the environment variable.Otherwise, you’ll get an error: Type 'undefined' is not assignable to type 'string'
   .then(() => app.listen(process.env.PORT, () => console.log(`Base MongoDB conectado, servidor corriendo en el puerto: http://localhost:${process.env.PORT}`)))
   .catch((error) => console.log(`${error} no se pudo conectar`));
 
-
-
-//asdf
+ 
