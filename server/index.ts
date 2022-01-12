dc63aae47fc454a691d128b50eaee2fe1df839ac
lauranuez <lauranuezzaragoza@gmail.com>
2022-01-12 17:59:03 +0100

    cambios sockets

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ import flatsRoutes from './routes/flats';
 import markersRoutes from './routes/markers';
 dotenv.config();
 
-//let users =Array<any>();
+let usersSocket =Array<any>();
 
 const app = express();
 
@@ -44,7 +44,7 @@ mongoose.connect(`${process.env.CONNECTION_URL}`)                         // hav
   .then(() => app.listen(process.env.PORT, () => console.log(`Base MongoDB conectado, servidor corriendo en el puerto: http://localhost:${process.env.PORT}`)))
 
   .catch((error) => console.log(`${error} no se pudo conectar`));
-/*
+
 const io = require("socket.io")(8900,{
   cors:{
       origin:"http://localhost:3000",
@@ -52,39 +52,58 @@ const io = require("socket.io")(8900,{
 });
 
 const addUser = (userId: string, socketId:string ) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
+  !usersSocket.some((user) => user.userId === userId) &&
+  usersSocket.push({ userId, socketId });
 };
 
 const removeUser = (socketId:string) => {
-  users = users.filter((user) => user.socketId !== socketId);
+  usersSocket = usersSocket.filter((user) => user.socketId !== socketId);
 };
 
 const getUser = (userId: string) => {
-  return users.find((user) => user.userId === userId);
+  return usersSocket.find((user) => user.userId === userId);
 };
 
 io.on("connection", (socket:Socket) => {
   //when connect
-  console.log("user connected");
-  
-  //take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-  });
-
-  //send and get message
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
+  console.log("a user connected.");
+    
+    try{
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      console.log(userId,socket.id);
+      io.emit("getUsers", usersSocket);
+      console.log(usersSocket);
     });
-});
+    }catch(e){
+      console.log(e);
+    }
+  
+    //send and get message
+    try{
+      socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        if (user){
+          io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+          });
+        }
+      });
 
-  //when disconnect
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-  });
-});*/
+    }catch(e){
+      console.log(e);
+    }
+    
+    try{
+    //when disconnect
+    socket.on("disconnect", () => {
+      console.log("a user disconnected!");
+      removeUser(socket.id);
+      io.emit("getUsers", usersSocket);
+    });
+    }catch(e){
+      console.log(e);
+    }
+});
